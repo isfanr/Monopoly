7fb8f6ed908d163d3bd23444b31213a2b15237fa
isfanr <38127568+isfanr@users.noreply.github.com>
2019-03-16 18:44:13 +0700

    Add files via upload

---
+++ Space.java
@@ -0,0 +1,14 @@
+public class Space extends Tile 
+{ 
+	public Space(int pos)
+	{
+		super(pos);
+	}
+}
+
+//untuk Go, setiap lewat program utama player nambah uang
+//untuk GoToJail, setiap lewat player langsung pindahin ke jail
+//untuk Jail, player lewat doang
+//untuk FreeParking player lewat doang
+//untuk Tax, player kena minus uang
+//untuk Community Chest dan Chance buat algoritma di program utama
