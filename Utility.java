5f109539fc885b928312ec5095f9ed8aa6c64844
Lorddeepwhite <45059291+Lorddeepwhite@users.noreply.github.com>
2019-03-17 20:17:27 +0700

    Add files via upload

---
+++ Utility.java
@@ -0,0 +1,32 @@
+public class Utility extends Property { 
+	protected long utilityPrice;  		//Harga beli dari Utility
+	protected int numOfUtility;  		//Jumlah jenis Utility yang dimiliki 
+	protected int utilityStatus; 		//Status kepemilikan Lot; 0 jika tidak ada yang memiliki dan 1, 2, 3 dst untuk menunjukan dimiliki oleh player ke n
+	
+	public Utility(int pos, long utilityPrice, int numOfUtility, int utilityStatus) { 
+		super(pos);
+		this.utilityPrice = utilityPrice; 
+		this.numOfUtility = 0; 
+		this.utilityStatus = 0; 
+	}
+	
+	public long getUtilityPrice() { 
+		return (this.utilityPrice); 
+	}
+	
+	public int getNumOfUtility() { 
+		return (this.numOfUtility);
+	}
+	
+	public int getUtilityStatus() {
+		return (this.utilityStatus); 
+	}
+	
+	public void buyUtility(int stat) { 
+		this.utilityStatus = stat; 
+	}
+	
+	public void sellUtility() { 
+		this.utilityStatus = 0; 
+	}
+}
