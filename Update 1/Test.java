4aa427b27cc82eea061145d59b748d0aa74bd20e
fadityad <48549673+fadityad@users.noreply.github.com>
2019-03-30 22:48:48 +0700

    Add files via upload

---
+++ Update 1/Test.java
@@ -0,0 +1,39 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Test {
+
+    static int counter = 1;
+	static Timer timer;
+    public static void main(String[] args) {
+
+        TimerTask timerTask = new TimerTask() {
+
+            public void run() {
+                counter++;           
+            }
+        };
+        
+        Thread t = new Thread(new Runnable() {
+
+            public void run() {
+                while (true) {
+                    try {
+						if (counter == 31) {
+                            System.out.println("Ganti pemain");
+                            counter = 1;
+                            run();
+                        }
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+			
+        timer = new Timer("Timer");
+        timer.scheduleAtFixedRate(timerTask, 0, 1000);
+        t.start();
+    }
+}
