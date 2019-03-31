5cab4f9b62dfde9c51c05a1b5b800a62f5c089c5
Adi <38127568+isfanr@users.noreply.github.com>
2019-04-01 00:01:15 +0700

    Add files via upload

---
+++ Update 3/Lot.java
@@ -0,0 +1,67 @@
+public class Lot extends Property { 
+	protected int lotPrice;  		//Harga beli dari Lot
+	protected int numOfHouse;  		//Jumlah rumah yang dimiliki di Lot tersebut 
+	protected int housePrice; 		//Harga beli rumah pada Lot
+	protected int lotStatus; 		//Status kepemilikan Lot; 0 jika tidak ada yang memiliki dan 1, 2, 3 dst untuk menunjukan dimiliki oleh player ke n
+	protected int clusterStatus; 	//Status yang menunjukan apakah cluster dari Lot ini dimiliki oleh 1 orang; 0 jika tidak dan 1 jika iya 
+	protected int clusterNum; 
+	
+	public Lot(int pos, int lotPrice, int housePrice, int clusterNum) { 
+		super(pos,1);
+		this.lotPrice = lotPrice; 
+		this.numOfHouse = 0; 
+		this.housePrice = housePrice;
+		this.lotStatus = 0; 
+		this.clusterStatus = 0; 
+		this.clusterNum = clusterNum;
+	}
+	
+	public int getLotPrice() { 
+		return (this.lotPrice); 
+	}
+	
+	public int getHouse() { 
+		return (this.numOfHouse);
+	}
+	
+	public int getHousePrice() { 
+		return (this.housePrice); 
+	}
+	
+	public int getLotStatus() {
+		return (this.lotStatus); 
+	}
+	
+	public int getClusterStatus() { 
+		return (this.clusterStatus); 
+	}
+	
+	public int getClusterNum() { 
+		return (this.clusterNum); 
+	}
+	
+	public void buyHouse(int house) { 
+		this.numOfHouse = house; 
+	}
+	
+	public void buyLot(int stat) { 
+		this.lotStatus = stat; 
+	}
+	
+	public void setClusterStatus(int stat) { 
+		this.clusterStatus = stat; 
+	}
+	
+	public void sellLot() { 
+		this.lotStatus = 0; 
+	}
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
---
+++ Update 3/Railroad.java
@@ -0,0 +1,22 @@
+public class Railroad extends Property { 
+	protected int railroadPrice;  		//Harga beli dari Railroad
+	protected int numOfRailroad;  		//Jumlah jenis Railroad yang dimiliki 
+	protected int railroadStatus; 		//Status kepemilikan Railroad; 0 jika tidak ada yang memiliki dan 1, 2, 3 dst untuk menunjukan dimiliki oleh player ke n
+	
+	public Railroad(int pos, int railroadPrice, int railroadStatus) { 
+		super(pos,2);
+		this.railroadPrice = railroadPrice; 
+		this.railroadStatus = 0; 
+	}
+	
+	
+	public long getRailroadPrice() { 
+		return (this.railroadPrice); 
+	}
+
+	
+	public int getRailroadStatus() {
+		return (this.railroadStatus); 
+	}
+
+}
