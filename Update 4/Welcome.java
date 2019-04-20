421bc2c451634bcb8ed8636bfc1682e3d753b6c1
Yudhistira Qasthari Putra <38126374+AsthaYQP@users.noreply.github.com>
2019-04-20 22:13:06 +0700

    Add files via upload

---
+++ Update 4/Welcome.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Welcome implements ActionListener { 
+	JFrame wFrame = new JFrame(); 
+	JPanel wPanel = new JPanel();
+	JLabel wLabel = new JLabel("Welcome to Monopoly");  
+	JButton wButton = new JButton("Start"); 
+	
+	Welcome() { 
+		WelcomePanel(); 
+	}
+	
+	public void WelcomePanel() { 
+		wFrame.setSize(300,300); 
+		wPanel.setBounds(50, 50, 200, 200); 
+		wPanel.add(wLabel); 
+		wPanel.add(wButton); 
+		wButton.addActionListener(this); 
+	}
+	
+	public void actionPerformed(ActionEvent e) { 
+		wFrame.dispatchEvent(new WindowEvent(wFrame, WindowEvent.WINDOW_CLOSING));
+	}
+}
+
+public class Main { 
+	public static void main(String[] args) { 
+		new Welcome(); 
+	}
+}
+		
