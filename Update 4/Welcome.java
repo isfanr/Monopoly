91ca70a61d706488a437e5c61282ec8dcf2ec790
fadityad <48549673+fadityad@users.noreply.github.com>
2019-04-21 17:35:14 +0700

    Update Welcome.java

--- Update 4/Welcome.java
+++ Update 4/Welcome.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+/*import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -21,6 +21,55 @@ class Welcome implements ActionListener {
 	
 	public void actionPerformed(ActionEvent e) { 
 		wFrame.dispatchEvent(new WindowEvent(wFrame, WindowEvent.WINDOW_CLOSING));
+	}
+}*/
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Welcome implements ActionListener { 
+	JFrame wFrame = new JFrame(); 
+	JPanel wPanel = new JPanel();
+	JLabel wLabel = new JLabel("Welcome to Monopoly"); 
+	JButton wButton = new JButton("START"); 
+	
+	public static void main(String[] args) {
+		new Welcome();
+	}
+	
+	public Welcome() { 
+		
+		wFrame.setSize(300,300); 
+		wFrame.getContentPane().setBackground(Color.black);
+		wFrame.setLayout(null);
+		wFrame.setVisible(true);		
+		
+		wPanel.setBounds(50, 50, 200, 200);
+		wFrame.add(wPanel);
+		
+		wLabel.setForeground(Color.black);
+		wPanel.add(wLabel); 		
+		
+		wButton.setBounds(275, 400, 60, 80);
+		wPanel.add(wButton);
+
+		wButton.addActionListener(this); 
+	}
+	
+	public void NewGame() {
+		
+		String nama1 = JOptionPane.showInputDialog(null, "Pemain 1: ", "New Game", JOptionPane.QUESTION_MESSAGE);		
+		String nama2 = JOptionPane.showInputDialog(null, "Pemain 2: ", "New Game", JOptionPane.QUESTION_MESSAGE);
+		String cmd = JOptionPane.showInputDialog(null, "Ketik 'MULAI' ", "New Game", JOptionPane.QUESTION_MESSAGE);
+	}
+	public void actionPerformed(ActionEvent e) { 
+		wFrame.dispatchEvent(new WindowEvent(wFrame, WindowEvent.WINDOW_CLOSING));
+		NewGame();
 	}
 }
 
